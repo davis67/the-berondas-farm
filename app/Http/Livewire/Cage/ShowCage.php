a228b91260c87fc64f3d054812e8452aac028c10
davis67 <davisag67@gmail.com>
2021-02-12 21:17:35 +0300

    fix merge conflicts

--- app/Http/Livewire/Cage/ShowCage.php
+++ app/Http/Livewire/Cage/ShowCage.php
@@ -8,103 +8,6 @@ use Livewire\Component;
 
 class ShowCage extends Component
 {
-<<<<<<< HEAD
-    /**
-     * The cage instance.
-     *
-     * @var mixed
-     */
-    public $cage;
-
-    /**
-     * The selected rabbit instance.
-     *
-     * @var mixed
-     */
-    public $selectRabbit;
-
-    /**
-     * Indicates if rabbit transfer is being confirmed.
-     *
-     * @var bool
-     */
-    public $confirmingRabbitTransfer = false;
-
-    /**
-     * The rabbit id.
-     *
-     * @var int
-     */
-    public $rabbit;
-
-    /**
-     * Mount the component.
-     *
-     * @param mixed $cage
-     *
-     * @return void
-     */
-    public function mount(Cage $cage)
-    {
-        $this->cage = $cage->loadMissing('rabbits');
-    }
-
-    /**
-     * Validate the transfer of the rabbit from one cage to another.
-     *
-     * @return void
-     */
-    public function validateTransfer()
-    {
-        $this->validate([
-            'rabbit' => ['required'],
-        ]);
-
-        $this->confirmingRabbitTransfer = true;
-    }
-
-    /**
-     * Handler the transfer of the rabbit from one cage to another.
-     *
-     * @return void
-     */
-    public function handleTransfer()
-    {
-        $this->cage->transferRabbit($this->rabbit);
-
-        $this->confirmingRabbitTransfer = false;
-
-        $this->rabbit = '';
-
-        session()->flash('success', 'Cage Info successfully updated.');
-
-        return redirect(route('cages.show', $this->cage->id));
-    }
-
-    /**
-     * Updating the rabbit instance.
-     *
-     * @return void
-     */
-    public function updatingRabbit($value)
-    {
-        if (! empty($value)) {
-            $this->selectRabbit = Rabbit::findOrFail($value);
-        }
-    }
-
-    /**
-     * Render the component.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function render()
-    {
-        return view('livewire.cage.show-cage', [
-            'rabbits' => Rabbit::latest()->get(),
-        ])->extends('layouts.app');
-    }
-=======
 	/**
 	 * The cage instance.
 	 *
@@ -168,12 +71,6 @@ class ShowCage extends Component
 	{
 		$this->cage->transferRabbit($this->rabbit);
 
-		$transferedRabbit = Rabbit::findOrFail($this->rabbit);
-
-		$this->rabbit->update([
-			'cage_id' => $this->cage->id,
-		]);
-
 		$this->confirmingRabbitTransfer = false;
 
 		$this->rabbit = '';
@@ -206,5 +103,4 @@ class ShowCage extends Component
 			'rabbits' => Rabbit::latest()->get(),
 		])->extends('layouts.app');
 	}
->>>>>>> dev
 }
